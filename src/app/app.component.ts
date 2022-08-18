64b1740e81f777db14a3d92a3da18c130a9dcb0f
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-18 19:45:57 -0300

    dados aula2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Alunos Dev For Tech';
+  public title = 'Alunos Dev For Tech';
 }
